b3cff697323dbfa14f374ce6bbe1c22f655699f5
Lawrence O. Caulker <locaulker@gmail.com>
2021-03-14 05:07:30 -0400

    added children props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,8 +1,6 @@
 import React from "react"
 
 const CommentDetail = (props) => {
-  console.log(props)
-
   return (
     <div className="comment">
       <a href="/" className="avatar">
--- src/index.js
+++ src/index.js
@@ -2,28 +2,42 @@ import React from "react"
 import ReactDOM from "react-dom"
 import faker from "faker"
 import CommentDetail from "./CommentDetail"
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post"
-        avatar={faker.random.image()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        content="I like the subject"
-        avatar={faker.random.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        content="I like the writing"
-        avatar={faker.random.image()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.random.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="I like the subject"
+          avatar={faker.random.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          content="I like the writing"
+          avatar={faker.random.image()}
+        />
+      </ApprovalCard>
     </div>
   )
 }
